96c66998b3b96d46ddd5174115c60b331547d8f1
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-21 11:54:36 +0430

    Lets start with STL widget and add material app and add some foolders

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:manager/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Manage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
