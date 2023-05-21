dc9e98daae308417e08c182290f3daaba2e20560
Mehranxt <132151347+mehranext@users.noreply.github.com>
2023-05-21 13:13:33 +0430

    for moving and active button we create a variable as name as int Current index and after that we use setState for active button

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
@@ -8,21 +9,24 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int CurrentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
- backgroundColor: Colors.white,
- bottomNavigationBar: AnimatedBottomNavigationBar(
-  inactiveColor: Colors.black54,
-  icons: const [Icons.home , Icons.info],
-      activeIndex: 0,
-      gapLocation: GapLocation.center,
-      notchSmoothness: NotchSmoothness.verySmoothEdge,
-      leftCornerRadius: 32,
-      rightCornerRadius: 32,
-      onTap: (index) {},
-
- ),
+      backgroundColor: Colors.white,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        inactiveColor: Colors.black54,
+        icons: const [Icons.home, Icons.info],
+        activeIndex: CurrentIndex,
+        gapLocation: GapLocation.center,
+        notchSmoothness: NotchSmoothness.verySmoothEdge,
+        leftCornerRadius: 32,
+        rightCornerRadius: 32,
+        onTap: (index) {
+          setState(() {});
+          CurrentIndex = index;
+        },
+      ),
     );
   }
 }
